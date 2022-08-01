1c9e7d29704fde7cbc7ca7c38a1c275c14276c8c
anton shabanov <89005421555@mail.ru>
2022-08-01 11:44:00 +0300

    edit way in appv

--- src/test/java/ru/netology/CashBackTest.java
+++ src/test/java/ru/netology/CashBackTest.java
@@ -30,8 +30,6 @@ public class CashBackTest {
     @BeforeEach
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
 
